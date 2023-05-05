a4fdf0c31491595e72aeb32e2fb4d652a20d119c
JonG <teflonjuan@icloud.com>
2023-05-05 03:05:46 -0500

    Got cards made and rendered for projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,40 +1,76 @@
-import React, { useState } from "react";
-import '../../styles/style.css'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import movieGame from '../../imgs/MovieGame.png';
+import dailyPlanner from '../../imgs/DailyPlanner.png';
+import noteTaker from '../../imgs/NoteTaker.png';
+import repairRadar from '../../imgs/RepairRadar.png';
+import teamManager from '../../imgs/TeamManager.png';
+import textEditor from '../../imgs/TextEditor.png';
 
+const Projects = () => {
 
-export default function Project({projectRepo, img, projectName, projectURL}) {
-  //Use States to store whether the picture is hovered or not
-    const [isHovered, setIsHovered] = useState(false);
+  const projectUrl = [
+    "https://jboyce313.github.io/movie-game/score-guesser.html",
+    "https://jonathanguhl.github.io/DailyPlanner/",
+    "https://jgs-note-taker.herokuapp.com/",
+    "https://repairradar.herokuapp.com/",
+    "https://github.com/JonathanGuhl/TeamManager",
+    "https://stark-cliffs-20391.herokuapp.com/"
+  ];
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
+  const projectRepo = [
+    "https://github.com/jboyce313/movie-game",
+    "https://github.com/JonathanGuhl/DailyPlanner",
+    "https://github.com/JonathanGuhl/Note-Taker",
+    "https://github.com/AmeliaBigler/RepairRadar",
+    "https://github.com/JonathanGuhl/TeamManager",
+    "https://github.com/JonathanGuhl/Text-Editor"
+  ];
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
+  const projectName = [
+    "Rotten Tomatoes Review Guesser",
+    "Daily Planner",
+    "Note Taker",
+    "Repair Radar",
+    "Team Manager",
+    "Text Editor"
+  ];
 
- 
+  const projectImg = [
+    movieGame,
+    dailyPlanner,
+    noteTaker,
+    repairRadar,
+    teamManager,
+    textEditor
+  ];
 
   return (
-    <div className="col-md-6">
-      <h3>{projectName}</h3>
-      <div className="position-relative" onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}>
-        
-        <img
-          src={img}
-          alt="Screenshot of Project"
-          className={`img-fluid ${isHovered ? "blur-image blur-effect" : "blur-image"}`}
-          
-        />
-        {isHovered && (
-          <div className="position-absolute bottom-50 start-0">
-            <p  className="badge" rel="noreferrer"></p>
-            <p  className="badge"  rel="noreferrer"></p>
-          </div>
-        )}
+    <div className="container">
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          {projectRepo.map((repo, index) => (
+            <div className="col" key={index}>
+              <Card className="h-100 projectCard">
+                <Card.Img
+                  variant="top"
+                  src={projectImg[index]}
+                  alt="Screenshot of Project"
+                />
+                <Card.Body className="projectCardBody">
+                  <Card.Title className="text-center projectCardBody">{projectName[index]}</Card.Title>
+                </Card.Body>
+                <Card.Body className="text-center projectCardBody">
+                  <Button className="projectButton custom-btn me-2" href={projectRepo[index]} target="_blank" rel="noreferrer">View Repository</Button>
+                  <Button className="projectButton custom-btn me-2" href={projectUrl[index]} target="_blank" rel="noreferrer">View Deployment</Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+
   );
 }
+
+export default Projects;
